bc3ab3adfcefd8f472d8f0ad480a1506fbd40753
Richard Schmitt <rschmitt@connectedway.com>
2024-09-08 09:18:28 -0500

    Update server to almost get clean shutown

--- smbserver.c
+++ smbserver.c
@@ -23,6 +23,8 @@
 
 #include "smbinit.h"
 
+static OFC_BOOL kill_me = OFC_FALSE;
+
 #define BUFFER_SIZE 80
 OFC_HANDLE hiThread = OFC_HANDLE_NULL;
 OFC_HANDLE hoThread = OFC_HANDLE_NULL;
@@ -30,8 +32,9 @@ OFC_HANDLE hoThread = OFC_HANDLE_NULL;
 static OFC_VOID term_handler(OFC_INT signal)
 {
   printf("Terminating SMB Server\n");
-  ofc_thread_delete(hiThread);
-  ofc_thread_delete(hoThread);
+  if (hiThread != OFC_HANDLE_NULL || hoThread != OFC_HANDLE_NULL)
+    printf("Waiting for Chat to Close.  Hit ^D\n");
+  kill_me = OFC_TRUE;
 }
 
 static OFC_DWORD ichatApp(OFC_HANDLE hThread, OFC_VOID *context)
@@ -50,31 +53,29 @@ static OFC_DWORD ichatApp(OFC_HANDLE hThread, OFC_VOID *context)
   if ((hFile == OFC_HANDLE_NULL) ||
       (hFile == OFC_INVALID_HANDLE_VALUE))
     {
-      printf ("Couldn't create ichatsvc file\n") ;
+      printf ("Couldn't create ichat\n") ;
     }
   else
     {
+      printf("Incoming Chat.  Hit ^D to terminate\n");
+
+      clearerr(stdin);
       while (!ofc_thread_is_deleting(hThread))
         {
 	  char *p;
+	  printf("> ");
           p = fgets(buffer, BUFFER_SIZE, stdin);
 	  if (p == NULL)
 	    {
-	      ofc_thread_delete(hThread);
-	    }
-	  else if (strlen(buffer)==0)
-	    {
-	      ofc_thread_delete(hThread);
-	    }
+	      printf("Hangup\n");
+	      ofc_thread_delete(hiThread);
+	      ofc_thread_delete(hoThread);
+ 	    }
 	  else if (OfcWriteFile(hFile, buffer, strlen(buffer),
 				OFC_NULL, OFC_HANDLE_NULL) == OFC_FALSE)
 	    {
-	      printf("Couldn't write to ichatsvcfile %d\n", OfcGetLastError());
-	      ofc_thread_delete(hThread);
-	    }
-	  else if (strncmp(buffer, "quit", 4) == 0)
-	    {
-	      printf("Exiting\n");
+	      printf("Couldn't write to ichat %d\n",
+		     OfcGetLastError());
 	      ofc_thread_delete(hiThread);
 	      ofc_thread_delete(hoThread);
 	    }
@@ -85,30 +86,32 @@ static OFC_DWORD ichatApp(OFC_HANDLE hThread, OFC_VOID *context)
 }
 static OFC_DWORD ochatApp(OFC_HANDLE hThread, OFC_VOID *context)
 {
-  OFC_HANDLE hFile ;
+  OFC_HANDLE hFile;
   OFC_CHAR buffer[BUFFER_SIZE];
 
   hFile = OfcCreateFileW (TSTR("IPC:/ochatsvc"),
-                          OFC_GENERIC_READ | OFC_GENERIC_WRITE,
-                          OFC_FILE_SHARE_READ,
-                          OFC_NULL,
-                          OFC_CREATE_ALWAYS,
-                          OFC_FILE_ATTRIBUTE_NORMAL,
-                          OFC_HANDLE_NULL) ;
+			   OFC_GENERIC_READ | OFC_GENERIC_WRITE,
+			   OFC_FILE_SHARE_READ,
+			   OFC_NULL,
+			   OFC_CREATE_ALWAYS,
+			   OFC_FILE_ATTRIBUTE_NORMAL,
+			   OFC_HANDLE_NULL) ;
 
   if ((hFile == OFC_HANDLE_NULL) ||
       (hFile == OFC_INVALID_HANDLE_VALUE))
     {
-      printf ("Couldn't create ochatsvc file\n") ;
+      printf ("Couldn't create ochat\n") ;
     }
   else
     {
       while (!ofc_thread_is_deleting(hThread))
         {
           OFC_DWORD bytes_read;
-          if (OfcReadFile(hFile, buffer, BUFFER_SIZE, &bytes_read, OFC_HANDLE_NULL) == OFC_FALSE)
+          if (OfcReadFile(hFile, buffer, BUFFER_SIZE,
+			  &bytes_read, OFC_HANDLE_NULL) == OFC_FALSE)
 	    {
-	      printf("Couldn't read from ochatsvcfile %d\n", OfcGetLastError());
+	      printf("Couldn't read from ochat %d\n",
+		     OfcGetLastError());
 	      ofc_thread_delete(hThread);
 	    }
           else
@@ -130,24 +133,46 @@ int main (int argc, char **argp)
 
   OFC_HANDLE hApp = of_smb_startup_server(OFC_HANDLE_NULL);
 
-  hiThread = ofc_thread_create(&ichatApp,
-			       OFC_THREAD_THREAD_TEST, OFC_THREAD_SINGLETON,
-			       OFC_NULL,
-			       OFC_THREAD_JOIN, OFC_HANDLE_NULL);
-  if (hiThread == OFC_HANDLE_NULL)
-    printf("Could not create ichatApp\n");
-
-  hoThread = ofc_thread_create(&ochatApp,
-                               OFC_THREAD_THREAD_TEST, OFC_THREAD_SINGLETON,
-                               OFC_NULL,
-                               OFC_THREAD_JOIN, OFC_HANDLE_NULL);
-  if (hoThread == OFC_HANDLE_NULL)
-    printf("Could not create ochatApp\n");
-
   ofc_process_term_trap(term_handler);
 
-  ofc_thread_wait(hiThread);
-  ofc_thread_wait(hoThread);
+  while (!kill_me)
+    {
+      printf("SpiritCloud Chat App.  Waiting for Incoming Connection...\n");
+
+      hiThread = ofc_thread_create(&ichatApp,
+				   OFC_THREAD_THREAD_TEST,
+				   OFC_THREAD_SINGLETON,
+				   OFC_NULL,
+				   OFC_THREAD_JOIN, OFC_HANDLE_NULL);
+      if (hiThread == OFC_HANDLE_NULL)
+	{
+	  printf("Could not create ichatApp\n");
+	  kill_me = OFC_TRUE;
+	}
+
+      hoThread = ofc_thread_create(&ochatApp,
+				   OFC_THREAD_THREAD_TEST,
+				   OFC_THREAD_SINGLETON,
+				   OFC_NULL,
+				   OFC_THREAD_JOIN, OFC_HANDLE_NULL);
+      if (hoThread == OFC_HANDLE_NULL)
+	{
+	  printf("Could not create ochatApp\n");
+	  kill_me = OFC_TRUE;
+	}
+
+      if (hiThread != OFC_HANDLE_NULL)
+	{
+	  ofc_thread_wait(hiThread);
+	  hiThread = OFC_HANDLE_NULL;
+	}
+
+      if (hoThread != OFC_HANDLE_NULL)
+	{
+	  ofc_thread_wait(hoThread);
+	  hoThread = OFC_HANDLE_NULL;
+	}
+    }
 
   of_smb_shutdown_server(hApp);
 
